47167c4776890b62a611476c1282b9fa730be1e0
Nirbhay Singh <nirbhaymahan100@gmail.com>
2024-10-22 18:21:11 +0530

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 
 app.get("/home", (req, res) => {
   res.json({ message: "our server is up" });
 });
-app.listen(3000, () => {
-  console.log("server started on port : 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`server started on port : ${process.env.PORT}`);
 });
